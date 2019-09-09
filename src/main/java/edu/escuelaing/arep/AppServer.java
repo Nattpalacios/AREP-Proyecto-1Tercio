a8e8c348e3f77907e4f6c83e722da2fc91d88075
Natalia Palacios <natalia.palacios-r@mail.escuelaing.edu.co>
2019-09-09 14:00:05 -0500

    Probando

--- src/main/java/edu/escuelaing/arep/AppServer.java
+++ src/main/java/edu/escuelaing/arep/AppServer.java
@@ -89,7 +89,7 @@ public class AppServer {
                 		String path = inputLine.split(" ")[1].split("/")[2];
                 		System.err.println("Path:" + path);
                 		System.err.println(System.getProperty("user.dir"));
-                		String direccion = System.getProperty("user.dir") + "/recursosWeb/" + path;
+                		String direccion = "/recursosWeb/" + path;
                 		outputLine = "HTTP/1.1 200 OK\r\n"
 	                            + "Content-Type: text/html\r\n"
 	                            + "\r\n";
